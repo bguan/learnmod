0f8bb877af173f274854c79171c937359b5d253d
bguan <brian.guan@gmail.com>
2021-03-28 01:26:42 -0700

    Added logic to check surrounding for goodies

--- src/main/java/xmod/XCommand.java
+++ src/main/java/xmod/XCommand.java
@@ -9,6 +9,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
@@ -29,12 +30,19 @@ public class XCommand {
 
 		LiteralArgumentBuilder<CommandSource> xCommand = Commands.literal("X")
 				.requires((commandSource) -> commandSource.hasPermissionLevel(1))
-				.executes((context) -> doCommand(context));
+				.executes((context) -> doX(context));
 
 		dispatcher.register(xCommand);
+
+		LiteralArgumentBuilder<CommandSource> gCommand = Commands.literal("G")
+				.requires((commandSource) -> commandSource.hasPermissionLevel(1))
+				.executes((context) -> doG(context));
+
+		dispatcher.register(gCommand);
+
 	}
 
-	static int doCommand(CommandContext<CommandSource> ctx) {
+	static int doX(CommandContext<CommandSource> ctx) {
 		putBlock(ctx, -2, 2, 2);
 		putBlock(ctx, -1, 2, 2);
 		putBlock(ctx, 0, 2, 2);
@@ -42,6 +50,22 @@ public class XCommand {
 		putBlock(ctx, 2, 2, 2);
 		return 0;
 	}
+
+	static int doG(CommandContext<CommandSource> ctx) {
+		int goodiesFound = 0;
+		for (int x = -100; x <= 100; x++) {
+			for (int y = -100; y <=100; y++) {
+				for (int z = -100; z <= 100; z++) {
+					if (isGoodie(ctx, x, y, z)) {
+						goodiesFound++;
+					}
+				}
+			}
+		}
+		LOGGER.info("Total Goodies found: "+goodiesFound);
+		return 0;
+	}
+
 
 	static void putBlock(CommandContext<CommandSource> ctx, int x, int y, int z) {
 		try {
@@ -57,4 +81,37 @@ public class XCommand {
 		}
 	}
 
+	static boolean isGoodie(CommandContext<CommandSource> ctx, int x, int y, int z) {
+		
+		Block[] goodies = { 
+			Blocks.GOLD_ORE, Blocks.GOLD_BLOCK, Blocks.NETHER_GOLD_ORE,
+			Blocks.DIAMOND_BLOCK, Blocks.DIAMOND_ORE,
+			Blocks.EMERALD_ORE, Blocks.EMERALD_BLOCK,
+			Blocks.LAPIS_ORE, Blocks.LAPIS_BLOCK,
+			Blocks.REDSTONE_ORE, Blocks.REDSTONE_BLOCK,
+			Blocks.OBSIDIAN, 
+			Blocks.CHEST, Blocks.ENDER_CHEST, Blocks.TRAPPED_CHEST,
+		};
+
+		try {
+			CommandSource source = ctx.getSource();
+			ServerWorld serverworld = source.getWorld();
+			BlockPos origin = source.asPlayer().getPosition();
+			BlockPos bpos = new BlockPos(origin.getX() + x, origin.getY() + y, origin.getZ() + z);
+			TileEntity entity = serverworld.getTileEntity(bpos);
+			if (entity == null) return false;
+			if (entity.getBlockState() == null) return false;
+			if (entity.getBlockState().getBlock() == null) return false;
+			Block bxyz = entity.getBlockState().getBlock();
+			for (Block gb : goodies) {
+				if (bxyz.equals(gb)) {
+					LOGGER.info("Found "+bxyz+" at "+bpos);
+					return true;
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.warn(e);
+		}
+		return false;
+	}
 }
